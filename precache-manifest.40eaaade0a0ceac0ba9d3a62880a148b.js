28cd3f9ba56fca11b16f3304a36b1c425bbd7ded
Xero321 <etemuujin789@gmail.com>
2020-06-12 00:11:38 +0900

    Updates

---
+++ precache-manifest.40eaaade0a0ceac0ba9d3a62880a148b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e42d4d9dae2f1764a58fb1c6b70da95",
+    "url": "/taniltsuulga/index.html"
+  },
+  {
+    "revision": "53bc135a16410a29e51f",
+    "url": "/taniltsuulga/static/css/main.04ba4fad.chunk.css"
+  },
+  {
+    "revision": "f964c6c3e259567b188d",
+    "url": "/taniltsuulga/static/js/2.6005baf7.chunk.js"
+  },
+  {
+    "revision": "e5999e63c33a32e77a192dc439bcb94e",
+    "url": "/taniltsuulga/static/js/2.6005baf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53bc135a16410a29e51f",
+    "url": "/taniltsuulga/static/js/main.1b5d278f.chunk.js"
+  },
+  {
+    "revision": "fcd7f40bd3712d51976b",
+    "url": "/taniltsuulga/static/js/runtime-main.c19ac69b.js"
+  },
+  {
+    "revision": "9ead811efadab1e1f1977b3c01b6e436",
+    "url": "/taniltsuulga/static/media/photo.9ead811e.jpg"
+  }
+]);
